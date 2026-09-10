81d114181156734919a65d8977751b01a922fede
soda-caustica <maxbeltran@protonmail.com>
2026-09-10 18:51:05 -0300

    Se creo agente DFS sin testear, y se agregó contador de agentes que llegaron al objetivo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import Deque, List
 from enum import Enum
 from random import randint
+from collections import deque
+
 class TipoCuadricula(Enum):
     CAMINABLE = 1
     MURO = 2
@@ -19,9 +21,10 @@ class Cuadricula:
 class Tablero:
     tablero : List[List[Cuadricula]]
     size : tuple[int,int] 
-    # x,y
-    def __init__(self, x : int,y : int):
-        self.size = (x,y)
+
+    def __init__(self, size: tuple[int,int]):
+        self.size = size
+        x,y = self.size
         self.tablero = []
         for i in range(x):
             self.tablero.append([])
@@ -56,8 +59,36 @@ class AgenteRandom(AgenteState):
         x,y = agente.pos
         posibles = contexto.tablero.tablero[x][y].vecinos
         siguiente = posibles[mov]
-        if siguiente is not None and siguiente.tipo == TipoCuadricula.CAMINABLE:
+        if siguiente is not None and siguiente.tipo is TipoCuadricula.CAMINABLE:
             agente.pos = siguiente.pos
+        if agente.pos == contexto.objetivo : agente.state = AgenteTermino()
+
+class AgenteDFS(AgenteState):
+    def update(self, agente: Agente, contexto: Juego):
+        cola : Deque[List[Cuadricula]]
+        cola = deque()
+        x,y = agente.pos
+        cola.append([contexto.tablero.tablero[x][y]])
+        caminoFinal = []
+        while cola:
+            camino = cola.pop()
+            nodo = camino[-1] ## el ultimo nodo en el camino es el actual
+            for vecino in nodo.vecinos:
+                if vecino is None or vecino.tipo is not TipoCuadricula.CAMINABLE: continue
+                if vecino.pos is juego.objetivo:
+                    camino.append(nodo)
+                    caminoFinal = camino
+                    break
+                nuevoCamino = camino.copy()
+                nuevoCamino.append(nodo)
+                cola.appendleft(nuevoCamino)
+            if caminoFinal is not None: break
+        if caminoFinal is not None and len(caminoFinal) >= 2: agente.pos = caminoFinal[1].pos
+        if agente.pos is juego.objetivo: agente.state = AgenteTermino()
+
+class AgenteTermino(AgenteState): # indica que el agente dejó de actuar
+    def update(self, agente, contexto):
+        pass
 
 class Agente:
     pos : tuple[int,int] #(x,y)
@@ -67,28 +98,33 @@ class Agente:
         self.pos = (x,y)
         self.state = state
 
-    def update(self,juego : Juego):
-        self.state.update(self,juego)
+    def update(self,contexto : Juego):
+        self.state.update(self,contexto)
 
 class Juego:
     tablero : Tablero
     agentes : List[Agente]
+    objetivo : tuple[int,int]
 
-    def __init__(self, x: int, y: int):
-        self.tablero = Tablero(x,y)
+    def __init__(self, pos: tuple[int,int], obj: tuple[int,int]):
+        self.tablero = Tablero(pos)
         self.agentes = []
+        self.objetivo = obj
 
     def step(self):
         for agente in self.agentes:
             agente.update(self)
         for i in self.tablero.tablero:
             for j in i:
-                count = len(list(filter(lambda x: x.pos == j.pos, self.agentes)))
-                print('_' if count == 0 else count ,end=' ')
+                count = len(list(filter(lambda x: x.pos is j.pos, self.agentes)))
+                print('_' if count is 0 else count ,end=' ')
+                
             print()
+        agentesListos = len(list(filter(lambda x: isinstance(x.state,AgenteTermino),self.agentes)))
+        print(f"Agentes listos = {agentesListos}")
         input()
 
-juego = Juego(5,5)
+juego = Juego((5,5), (1,0))
 
 for i in range(3):
     x = randint(0,4)
