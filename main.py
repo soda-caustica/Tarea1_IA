71a284f0720291996d79b05b850c10a3cb7cd1dd
soda-caustica <maxbeltran@protonmail.com>
2026-09-10 19:14:35 -0300

    Agente DFS terminado (es bien tonto)

--- main.py
+++ main.py
@@ -54,37 +54,65 @@ class AgenteState:
         "XDE"
 
 class AgenteRandom(AgenteState):
+
     def update(self,agente: Agente, contexto : Juego):
         mov = randint(0,3)
         x,y = agente.pos
         posibles = contexto.tablero.tablero[x][y].vecinos
         siguiente = posibles[mov]
-        if siguiente is not None and siguiente.tipo is TipoCuadricula.CAMINABLE:
+        if siguiente is not None and siguiente.tipo == TipoCuadricula.CAMINABLE:
             agente.pos = siguiente.pos
         if agente.pos == contexto.objetivo : agente.state = AgenteTermino()
 
 class AgenteDFS(AgenteState):
+    def __init__(self):
+        self.camino: List[Cuadricula] = []
+        self.camino_idx: int = 0
+
     def update(self, agente: Agente, contexto: Juego):
-        cola : Deque[List[Cuadricula]]
-        cola = deque()
-        x,y = agente.pos
-        cola.append([contexto.tablero.tablero[x][y]])
-        caminoFinal = []
-        while cola:
-            camino = cola.pop()
-            nodo = camino[-1] ## el ultimo nodo en el camino es el actual
+        if not self.camino:
+            self.camino = self._buscar_camino(agente, contexto)
+            self.camino_idx = 0
+
+        if not self.camino:
+            agente.state = AgenteTermino()
+            return
+
+        self.camino_idx += 1
+        if self.camino_idx < len(self.camino):
+            agente.pos = self.camino[self.camino_idx].pos
+
+        if agente.pos == contexto.objetivo:
+            agente.state = AgenteTermino()
+
+    def _buscar_camino(self, agente: Agente, contexto: Juego) -> List[Cuadricula]:
+        inicio = contexto.tablero.tablero[agente.pos[0]][agente.pos[1]]
+        objetivo = contexto.objetivo
+
+        pila: Deque[List[Cuadricula]] = deque()
+        pila.append([inicio])
+        visitados = {inicio.pos}
+
+        while pila:
+            camino = pila.pop()
+            nodo = camino[-1]
+
+            if nodo.pos == objetivo:
+                return camino  
+
             for vecino in nodo.vecinos:
-                if vecino is None or vecino.tipo is not TipoCuadricula.CAMINABLE: continue
-                if vecino.pos is juego.objetivo:
-                    camino.append(nodo)
-                    caminoFinal = camino
-                    break
-                nuevoCamino = camino.copy()
-                nuevoCamino.append(nodo)
-                cola.appendleft(nuevoCamino)
-            if caminoFinal is not None: break
-        if caminoFinal is not None and len(caminoFinal) >= 2: agente.pos = caminoFinal[1].pos
-        if agente.pos is juego.objetivo: agente.state = AgenteTermino()
+                if vecino is None:
+                    continue
+                if vecino.tipo is not TipoCuadricula.CAMINABLE:
+                    continue
+                if vecino.pos in visitados:
+                    continue
+
+                visitados.add(vecino.pos)
+                nuevo_camino = camino + [vecino]
+                pila.append(nuevo_camino)
+
+        return []
 
 class AgenteTermino(AgenteState): # indica que el agente dejó de actuar
     def update(self, agente, contexto):
@@ -94,9 +122,9 @@ class Agente:
     pos : tuple[int,int] #(x,y)
     state : AgenteState
 
-    def __init__(self,x,y, state = AgenteRandom()) -> None:
+    def __init__(self,x,y, state : AgenteState = AgenteRandom()) -> None:
         self.pos = (x,y)
-        self.state = state
+        self.state = state 
 
     def update(self,contexto : Juego):
         self.state.update(self,contexto)
@@ -122,6 +150,8 @@ class Juego:
             print()
         agentesListos = len(list(filter(lambda x: isinstance(x.state,AgenteTermino),self.agentes)))
         print(f"Agentes listos = {agentesListos}")
+        print(f"Agentes restantes = {len(self.agentes) - agentesListos}")
+        print(f"Posicion DFS = {list(filter(lambda x: isinstance(x.state,AgenteDFS),self.agentes)).pop().pos}")
         input()
 
 juego = Juego((5,5), (1,0))
@@ -130,6 +160,7 @@ for i in range(3):
     x = randint(0,4)
     y = randint(0,4)
     juego.agentes.append(Agente(x,y))
+juego.agentes.append(Agente(randint(0,4),randint(0,4),AgenteDFS()))
 
 while True:
     juego.step()
